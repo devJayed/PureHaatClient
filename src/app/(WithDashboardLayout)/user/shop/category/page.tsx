65b4f8d27284810a4c13489b3c794194b35ec3a3
devJayed <jayedhossain.dev@gmail.com>
2025-11-09 20:39:59 +0600

    Fixing category undefined issue

--- src/app/(WithDashboardLayout)/user/shop/category/page.tsx
+++ src/app/(WithDashboardLayout)/user/shop/category/page.tsx
@@ -2,7 +2,9 @@ import ManageCategories from "@/components/modules/shop/category";
 import { getAllCategories } from "@/services/Category";
 
 const ProductCategoryPage = async () => {
-  const { data, meta } = await getAllCategories();
+  // const { data, meta } = await getAllCategories();
+  const res = await getAllCategories();
+  const data = res?.data || []; // ✅ fallback if undefined
   // console.log({data});
   return (
     <div>
