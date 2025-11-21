93f0ab59ee96ea9f781919f92b85a44050ae1212
devJayed <jayedhossain.dev@gmail.com>
2025-11-21 14:58:31 +0600

    Fixing layout calling layout with props

--- src/app/(WithCommonLayout)/layout.tsx
+++ src/app/(WithCommonLayout)/layout.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
 import Navbar2 from "@/components/shared/Navbar2";
+ import { getAllCategories } from "@/services/Category";
 
-const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+const CommonLayout = async ({ children }: { children: React.ReactNode }) => {
+  const { data: categories } = await getAllCategories();
   return (
     <>
-      <Navbar />
+      <Navbar categories={categories} />
       <Navbar2/>
       <main className="min-h-screen">{children}</main>
       <Footer />
